fea3305158a8bbb0acfb1127b6a762b9499f17f4
Muhammad Bial <mb973768@gmail.com>
2025-08-06 15:31:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,597 @@
+
+                           //   javascript start  first class //
+// var a = ("what your name")
+// alert(a)
+
+   
+// var a = 10
+// console.log(a)
+
+// a = 20
+// console.log(a)
+
+// var b = ("Abc")
+// console.log(b)
+
+// b = ("xyz")
+// console.log(b)
+
+// let a = 10
+// console.log(a)
+
+// let a = 20
+// console.log (a)
+
+// let a
+// console.log(a)
+
+// a = 10
+// console.log(a)
+
+// const a = 10
+// console.log(a)
+
+// a = 20
+// console.log(a)
+
+// let firstName = "Muhammad"
+// let lastName = "Bilal"
+
+// let fullName = firstName + " " + lastName
+// console.log(fullName)
+
+
+// let a = 5
+
+// let b = a
+
+//  console.log(a)
+
+// let firstName = prompt("Enter your name")
+
+// console.log(firstName)
+
+// let firstName = prompt ("firstname")
+// console.log(firstName)
+// let lastName = prompt ("lastname")
+// console.log(lastName)
+// let email = prompt ("email")
+// console.log(email)
+// let contact = prompt ("contact")
+// console.log(contact)
+    
+// let math = +prompt("math mark")
+// let science = +prompt("science mark")
+// let English = +prompt("english mark")
+// let urdu = +prompt("urdu")
+// let totalMark = math + science + English +  urdu
+
+// let totalposibleMark = 300;
+// let percentage = (totalMark / totalposibleMark) * 100
+// console.log("totalMark " + totalMark );
+// console.log("percentage " + percentage + "%") ;
+
+// let originalPrice = 2500
+// let discount = originalPrice * 0.2
+// let finalPrice = originalPrice -  discount
+
+// console.log ("discount value;" , discount)
+// console.log ("Final price after discount :" , finalPrice)
+  
+// if(age == 18){}
+// if(age != 18){}
+// if(age > 18){}
+// if(age < 18){}
+// if(age >= 18){}
+// if(age <= 18){}
+
+// let age = 50
+
+// if(age != 10){
+//     alert("student Allowed")
+// }
+
+// let age = 12
+
+// if( age >= 18){
+//    alert("Allowed")
+// }else if(StudentCard === true){
+//     alert("allowed")
+// }else {
+//     alert("not allowed")
+// }
+// let age = 10
+// let studentcard = true
+// if (age >= 18 && studentcard)
+// {
+//     console.log("Allow")
+// }
+
+// let mark = prompt("marks")
+// if(grade > 91){
+//    alert("grade A")
+// }
+// let studentName = ['Muhammad Bilal' , 'Muhammad Hassan','Muhammad S A W']
+// console.log(studentName )
+ 
+// let marks = 45
+ 
+// marks > 90 ? console.log("A") : marks > 80 ? console.log("B"): marks > 70 ? console.log("c"): marks > 60 ? console.log("D") :
+// console.log ("fail")
+
+// let studentName = [
+//     "saad" , 
+//     "bilal" ,
+//      "Ali" , 
+//      "khan" ,
+//       "Muhammad" ,
+//        "wahad" ,
+//         "subhan"
+// ]
+// for (let i = 0; i <  studentName .length; i++ ){
+//     console.log( studentName[i])
+
+
+// let a = "Aslam"
+// if (a === studentName[i]){
+//     console.log("yes Name was Match")
+// }
+// }
+
+// function  showName(){
+//     console.log("Aslamualikum Bilal");
+// }
+//  showName();
+
+// let a = Math.ceil()
+// a = Math.ceil(100.1) 
+// console.log(a)
+
+
+
+// let dateTime = new Date()
+// console.log(dateTime)
+// console.log(typeof dateTime)
+
+// let currentDate = dateTime.getDate()
+// console.log(currentDate)
+
+// let currentMonth = dateTime.getMonth()
+// console.log(currentMonth)
+ 
+// let currentyear = dateTime.getFullYear()
+// console.log(currentyear)
+
+// console.log(dateTime.getHours())
+// console.log(dateTime.getMinutes())
+
+// let now = new Date()
+// let dateofBirth = new Date ("2-Aug-2005")
+
+// console.log(now)
+// console.log(dateofBirth)
+
+
+
+// console.log(dateofBirth.getDate())
+// console.log(dateofBirth.getMonth())
+// console.log(dateofBirth.getFullYear())
+// console.log(dateofBirth.getMinutes())
+// console.log(dateofBirth.getHours())
+// console.log(dateofBirth.getMilliseconds())
+// console.log(dateofBirth.getTime()) 
+// console.log(dateofBirth.getSeconds())
+// console.log(dateofBirth.getTimezoneOffset())
+
+// let d = new Date ()
+
+// d.setDate(1)
+// d.setMonth(2)
+// d.setHours(4)
+// console.log(d)
+
+// let now = new Date()
+
+// let before = new Date ()
+
+
+// function timeBatao() {
+//   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+//                   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  
+//   const dt = new Date();
+//   const hours   = dt.getHours();
+//   const minutes = dt.getMinutes();
+//   const seconds = dt.getSeconds();
+//   const today   = dt.getDate();
+//   const month   = months[dt.getMonth()];   // ← [] not {}
+//   const year    = dt.getFullYear();
+
+//   // Screen par dikha do
+//   console.log(
+//     `${today} ${month} ${year} — ${hours}:${minutes}:${seconds}`
+//   );
+// }
+
+// timeBatao();
+
+//   function discount(price) {
+//     let a = price - price * 0.20 ;
+
+//     console.log(a)
+//   }
+
+
+// discount(30)
+// discount(50)
+// discount(70)
+
+
+// function abc(Hello) {
+    
+//     console.log("parameter value" , Hello);
+// }
+// abc("Hello");
+
+
+// function checkEven(num) {
+//     let a = num % 2
+//     console.log(a)
+//     if(a){
+//         console.log("odd")
+//     } else{
+//         console.log("Even")
+//     }
+// }
+
+// function getvalue() {
+//     return false
+// }
+// let a = getvalue()
+// console.log(a)
+
+// function add(a,b) {
+//     return a+b
+
+// }
+// let a = add(20,30)
+// let b = add(3333, 433 )
+//   console.log(a,b)
+
+//   convert gm to kg 
+// function kgtogm (gm) {
+//     return gm / 1000 
+// }
+// let valuegm = kgtogm(4000 )
+//   console.log(valuegm +'gm')
+
+// //   square a number
+
+// function square(num) {
+//     return num * num
+    
+// }
+// let a = square(40)
+// console.log(a)
+
+// // Count the number of Characters in a string
+
+// function string(str) {
+//     return str.length
+// }
+
+// reverse to string
+// check if a string  is  palindrome
+
+
+
+
+// capitalize the first leter
+
+// function caps(){
+//     let a = str.slice(0,1).touppercase()
+//     let b = str.slice(1).toLowercase()
+
+// return a+b
+// }
+
+//     let st = 3
+
+//     switch(st){
+// case 1:
+//         console.log("user is Approved");
+//         break;
+//  case 2:
+//         console.log("user is UnApproved");
+//             break;
+//  case 3:
+// case 4:
+//         console.log("user Request is Pending");
+//             break;
+
+        
+// case 5:
+//          console.log("user Reqquest is Rejected");
+//                     break;
+//   default:
+//         console.log("status is Uncorrect")
+//     }
+
+// let i = 0;
+// while(i < 10){    
+//     console.log("Bilal" + i)
+//     i++
+// }
+  // let i = 0;
+  // do{
+  //   console.log('Index ${i}')
+  //   i++
+  // }while (i < 0) 
+    
+    //  function button(massage){
+    //   console.log(massage)
+    // }
+
+    // let a = ""
+
+    // function button(massage){
+    //   a +=  massage
+    //   console.log(a)
+
+    // }
+    // function calculate(){
+    //   let answar = eval(a)
+    //   console.log(answar)
+    // }
+
+    // function Eventfire(massage){
+    //  console.log(massage)
+    // }
+  
+    // function mouseover(img){
+    //   console.log("mouseover")
+    //   img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR132TBAD0-GhGhN8_2Xr-3obkFd4zFbk6g&s"
+    // }
+
+    // function mouseout(img){
+    //   console.log("mouseout")
+    
+    // img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR132TBAD0-GhGhN8_2Xr-3obkFd4NzFbk6Hg&s"
+    // }
+  // function eventfire(massage){
+  //   console.log(massage)
+  // }
+  // function border(elem){
+  //   elem.className = "para border"
+  // }
+
+//   function border(elem){
+// elem.style.color = "darkcyan"
+// elem.style.backgroundColor = "White"
+// elem.style.border = "2px solid darkcyan"
+// elem.style.display = "none"
+       
+//   }   
+
+// function hidepera(){
+//   let p = document.getElementById("p")
+//   p.style.display = "none"
+// }
+
+function  square(){
+  box.className = "square"
+}
+function  Rectangle(){
+  box.className = "Rectangle"
+}
+function  circle(){
+  box.className = "circle"
+}
+
+// let images = [
+//   ""
+// ]
+
+// let sliderImage = document.getElementById("sliderImage")
+// let imageIndex = 0
+
+// function renderImage(){
+//   sliderImage.scr = image[imageIndex]
+// }
+// renderImage()
+// function nextImage(){
+//   if(imageIndex + 1 === images.length){
+//     imageIndex = 0;
+//   }else{
+//   imageIndex = 0
+//   }
+// }
+// function perviousImage(){
+//   if(imageIndex === 0)
+//     images
+// }
+
+//  let day = document.getElementById("day")
+
+// function submitText() {
+//   console.log("day.value")
+
+//   switch (day.value) {
+//     case "mon":
+//       console.log("phr se office jao")
+// break
+//       case "tue":
+//         console.log("tue ko bhi jao")
+// break
+//         default:
+//           console.log("kuch bhi nhi chalai ga")
+//   }
+// }
+
+function clickbtn(elem){
+  let a =  "https://img-corp.com/images/logo@2x.png";
+  console.log(elem.src);
+
+  elem.src = a;
+}
+
+// let msg = document.getElementById("msg")
+
+// function checkInput(inp){
+//   if(inp.value.length != 11){
+//     msg.innerHTML = "Input is not correct, please Enter 11 character value"
+//   }else{
+//     msg.innerHTML = ""
+//   }
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a = "Muhammad Bilal";
+// console.log(a.toUpperCase());
+
+
+// let text = "Muhammad Bilal";
+// let part = text.slice(0,10)
+// console.log(part)
+
+// let text1 = "Hamaza" 
+// console.log(text1.length)
+
+// let userInput = prompt("Umar");
+// let length = userInput.length;
+// console.log( length );
+
+
+// let password = prompt("Muhammad123:");
+
+// if(password.length >= 8) {
+//     console.log("password theek hai")
+// } else{
+//     console.log("password choot hai")
+// }
+
+// let text = "Saylani Mass Training";
+// console.log(text.includes("a")); // true
+
+
+// let name = "Muhammad Bilal";
+// console.log(name.includes("Muhammad"));
+
+
+// let gmail = "Bilal@gmail.com";
+// console.log(gmail.includes("@"))
+
+
+// let saylani = "saylani is a free of cost course";
+
+// if(text.includes("free")) {
+//     console.log("yes!   free of cost " )
+// } else{
+//     console.log("no! no free of cost");
+// }
+
+
+// 
+
+// let text1 = "Saylani offers Free IT courses";
+
+// if (text.toLowerCase().includes("Free")) {
+//   console.log("Yes! 'free' is in the sentence ✅");
+// } else {
+//   console.log("No! 'free' not found ❌");
+// }
+
+
+// let text2 = "saylani" ;{
+// let Characters = text.charAt(6)
+// console.log(text[5]);
+// }
+// let Name = "  Bilal   ";
+// let cleanName =  Name.trim();
+// console.log(cleanName);
+
+
+// let username = "Muhammad";
+
+// if (username.trim() === "Muhammad") {
+//   console.log("Correct Username");
+// }else{
+//     console.log("not correct")
+// }
+
+
+// let num = 4.3;
+// console.log(Math.round(num));
+
+// let randomNumber = Math.floor(Math.random() * 10)+1;
+// console.log(randomNumber);
+
+
+
+// let a = "1234.56";
+// let num1 = parseInt(a);
+// console.log(num1+ 1)
+
+
+
+// let num2 = 123;
+// let str = String(num);
+// console.log("Your score is " + str);
+
+// let num5 = 4.12345;
+// let result = num5.toFixed(2);
+// console.log(result)
+
+
+// let total = 60;
+// let obtained = 40;
+// let persentage = (obtained/total)* 100
+// console.log(persentage.toFixed(1))
+
+// let word = "saylani"
+// console.log(word[6])
+
+
+
+
+
+
+
